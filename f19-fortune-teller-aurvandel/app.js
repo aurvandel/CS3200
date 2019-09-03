51332a420a63fa4d9b3342fab490ccb22c05a455
aurvandel <parkergw@gmail.com>
2019-09-03 10:40:12 -0600

    finished, ready to submit

--- f19-fortune-teller-aurvandel/app.js
+++ f19-fortune-teller-aurvandel/app.js
@@ -1,74 +1,10 @@
-// temp data structures for random picks
-var boyNames = [
-    {
-        "name": "Liam",
-        "gender": "M",
-        "n": "19837"
-    },
-    {
-        "name": "Noah",
-        "gender": "M",
-        "n": "18267"
-    },
-    {
-        "name": "William",
-        "gender": "M",
-        "n": "14516"
-    },
-    {
-        "name": "James",
-        "gender": "M",
-        "n": "13525"
-    },
-    {
-        "name": "Oliver",
-        "gender": "M",
-        "n": "13389"
-    },
-    {
-        "name": "Benjamin",
-        "gender": "M",
-        "n": "13381"
-    },
-    {
-        "name": "Elijah",
-        "gender": "M",
-        "n": "12886"
-    }
-];
+// TODO: Get the entire list changed for uploading
 
-var girlNames = [
-    {
-        "name": "Emma",
-        "gender": "F",
-        "n": "18688"
-    },
-    {
-        "name": "Olivia",
-        "gender": "F",
-        "n": "17921"
-    },
-    {
-        "name": "Ava",
-        "gender": "F",
-        "n": "14924"
-    },
-    {
-        "name": "Isabella",
-        "gender": "F",
-        "n": "14464"
-    },
-    {
-        "name": "Sophia",
-        "gender": "F",
-        "n": "13928"
-    },
-    {
-        "name": "Charlotte",
-        "gender": "F",
-        "n": "12940"
-    }
-];
+var boyNamesURL = "https://api.myjson.com/bins/brlyf"
+var girlNamesURL = "https://api.myjson.com/bins/do73b"
+
+var boyNames = [];
+var girlNames = [];
 
 // query buttons
 var boyButton = document.querySelector("#boyButton");
@@ -82,9 +18,43 @@ var girlPick = document.querySelector("#girlName")
 boyButton.onclick = function () {
 	var randomBoy = Math.floor(Math.random() * boyNames.length)
 	boyPick.innerHTML = boyNames[randomBoy].name;
-}
+    
+    // Place in history list
+    var boyHistoryList = document.querySelector("#boyNameList");
+    var newBoyListItem = document.createElement("li");
+    newBoyListItem.innerHTML = boyNames[randomBoy].name;
+    boyHistoryList.appendChild(newBoyListItem);
+};
 
 girlButton.onclick = function () {
 	var randomGirl = Math.floor(Math.random() * girlNames.length)
 	girlPick.innerHTML = girlNames[randomGirl].name;
-}
+    
+    // place in history list
+    var girlHistoryList = document.querySelector("#girlNameList");
+    var newListItem = document.createElement("li");
+    newListItem.innerHTML = girlNames[randomGirl].name;
+    girlHistoryList.appendChild(newListItem);
+};
+
+
+// request the data from the server for the boy names:
+fetch(boyNamesURL).then(function (response) {
+  // parse (unpackage) the data from the server:
+  response.json().then(function (data) {
+    // (data is a list of objects)
+    // save the data for use later (when the button is clicked):
+    boyNames = data;
+    });
+  });
+
+// request the data from the server for the boy names:
+fetch(girlNamesURL).then(function (response) {
+  // parse (unpackage) the data from the server:
+  response.json().then(function (data) {
+    // (data is a list of objects)
+    // save the data for use later (when the button is clicked):
+    girlNames = data;
+    });
+  });
+
